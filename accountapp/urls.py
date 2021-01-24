6833c40e63fc39d9ab4734389b85c6ba85ceadf1
jun <bnmva23@hanmail.net>
2021-01-24 23:41:39 +0900

    21장

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from accountapp.views import AccountCreateView
 from django.urls import path
 from accountapp.views import hello_world
 
@@ -5,4 +6,6 @@ app_name = "accountapp"  # 한번에 리디렉트
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world')  # name 라우트에 대한 이름
+    # name 라우트에 대한 이름
+    , path('create/', AccountCreateView.as_view(), name='create')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,10 @@
-from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse, reverse_lazy
 from django.http.response import HttpResponseRedirect
 from accountapp.models import HelloWolrd
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
 
 
 def hello_world(request):
@@ -16,3 +19,10 @@ def hello_world(request):
     else:
         hello_world_list = HelloWolrd.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
