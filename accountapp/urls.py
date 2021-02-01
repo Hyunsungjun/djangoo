ff55e9c36541911a395daf17e5c609ee4d1b47bc
jun <bnmva23@hanmail.net>
2021-02-01 21:19:33 +0900

    22강

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,12 @@
-from accountapp.views import AccountCreateView
+from accountapp.views import AccountCreateView, hello_world
 from django.urls import path
-from accountapp.views import hello_world
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = "accountapp"  # 한번에 리디렉트
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')  # name 라우트에 대한 이름
-    # name 라우트에 대한 이름
-    , path('create/', AccountCreateView.as_view(), name='create')
+    path('hello_world/', hello_world, name='hello_world')  # name
+    , path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
